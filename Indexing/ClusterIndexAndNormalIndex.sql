dc936446e4f39d96e0fa56de35bc2b87b8089d72
mrunal93 <mrn26893@gmail.com>
2020-10-16 08:08:57 +0530

    [Mrunal]Add: Non-Clustring Indexing

--- Indexing/ClusterIndexAndNormalIndex.sql
+++ Indexing/ClusterIndexAndNormalIndex.sql
@@ -1,6 +1,8 @@
- drop index TemporaryMIS.PK__Temporar__3213E83FB5436F22;
+
+drop index TemporaryMIS.PK__Temporar__3213E83FB5436F22;
 
  create index IX_TemporaryMis_FileChange on TemporaryMIS (files_changed desc)
 
  create clustered index IX_TemporaryMis_FileChange_technology
  on TemporaryMIS (files_changed desc,technology asc)
+
---
+++ Indexing/NonClustringIndexing.sql
@@ -0,0 +1,14 @@
+ Create NonClustered index IX_TemporartMis_CpuCount 
+ on TemporaryMIS(Cpu_Count)
+
+ Create NonClustered index IX_TemporartMis_disk_used_memory
+ on TemporaryMIS(disk_used_memory)
+
+ Create NonClustered index IX_keyboard 
+ on TemporaryMIS(keyboard)
+
+ Create NonClustered index IX_technology 
+ on TemporaryMIS(technology)
+
+ Create NonClustered index IX_username 
+ on TemporaryMIS(user_name)
